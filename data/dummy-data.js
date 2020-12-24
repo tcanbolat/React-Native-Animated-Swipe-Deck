392688675e90290c048da8ea3ec1a51f902b0660
atcanbolat <kangalcanbolat@gmail.com>
2020-12-24 03:50:01 -0800

    created Dog model and added dummy-data file

---
+++ data/dummy-data.js
@@ -0,0 +1,50 @@
+import Dog from "../models/Dog";
+
+const dogData = [
+  new Dog("1", "Shetland", [
+    "https://images.dog.ceo/breeds/sheepdog-shetland/n02105855_14126.jpg",
+    "https://images.dog.ceo/breeds/sheepdog-shetland/n02105855_14653.jpg",
+    "https://images.dog.ceo/breeds/sheepdog-shetland/n02105855_15506.jpg",
+  ]),
+  new Dog("2", "Blood Hound", [
+    "https://images.dog.ceo/breeds/hound-blood/n02088466_10506.jpg",
+    "https://images.dog.ceo/breeds/hound-blood/n02088466_11398.jpg",
+    "https://images.dog.ceo/breeds/hound-blood/n02088466_1473.jpg",
+    "https://images.dog.ceo/breeds/hound-blood/n02088466_1563.jpg",
+    "https://images.dog.ceo/breeds/hound-blood/n02088466_10545.jpg",
+  ]),
+  new Dog("3", "Irish Spaniel", [
+    "https://images.dog.ceo/breeds/spaniel-irish/n02102973_1714.jpg",
+    "https://images.dog.ceo/breeds/spaniel-irish/n02102973_1822.jpg",
+    "https://images.dog.ceo/breeds/spaniel-irish/n02102973_2153.jpg",
+    "https://images.dog.ceo/breeds/spaniel-irish/n02102973_2453.jpg",
+    "https://images.dog.ceo/breeds/spaniel-irish/n02102973_2595.jpg",
+    "https://images.dog.ceo/breeds/spaniel-irish/n02102973_3326.jpg",
+  ]),
+  new Dog("4", "Malamute", [
+    "https://images.dog.ceo/breeds/malamute/n02110063_11034.jpg",
+    "https://images.dog.ceo/breeds/malamute/n02110063_17138.jpg",
+    "https://images.dog.ceo/breeds/malamute/n02110063_19390.jpg",
+    "https://images.dog.ceo/breeds/malamute/n02110063_17533.jpg",
+  ]),
+  new Dog("5", "Labrador", [
+    "https://images.dog.ceo/breeds/labrador/n02099712_2938.jpg",
+    "https://images.dog.ceo/breeds/labrador/n02099712_3364.jpg",
+  ]),
+  new Dog("6", "beagle", [
+    "https://images.dog.ceo/breeds/malamute/n02110063_17548.jpg",
+  ]),
+  new Dog("7", "Pug", [
+    "https://images.dog.ceo/breeds/pug/n02110958_11977.jpg",
+    "https://images.dog.ceo/breeds/pug/n02110958_12807.jpg",
+    "https://images.dog.ceo/breeds/pug/n02110958_13002.jpg",
+  ]),
+  new Dog("8", "newfoundland", [
+    "https://images.dog.ceo/breeds/newfoundland/n02111277_12042.jpg",
+    "https://images.dog.ceo/breeds/newfoundland/n02111277_14601.jpg",
+    "https://images.dog.ceo/breeds/newfoundland/n02111277_2255.jpg",
+    "https://images.dog.ceo/breeds/newfoundland/n02111277_2769.jpg",
+  ]),
+];
+
+export default dogData;
---
+++ models/Dog.js
@@ -0,0 +1,9 @@
+class Dog {
+  constructor(id, breed, images) {
+    this.id = id;
+    this.breed = breed;
+    this.images = images;
+  }
+}
+
+export default Dog;
